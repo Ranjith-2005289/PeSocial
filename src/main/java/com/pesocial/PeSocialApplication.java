ac300a7eb34b3e1dd732d36fb0bdd555b7dcf520
Ranjith Kumar N <rajkumar@Ranjiths-MacBook-Pro.local>
2026-03-27 23:19:29 +0530

    Initial commit: Spring Boot app on Java 25

---
+++ src/main/java/com/pesocial/PeSocialApplication.java
@@ -0,0 +1,13 @@
+package com.pesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesocial/PeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
